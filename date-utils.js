744d0acd63b9183bc908a8095446b0cafe9ae575
Gaoxiang <512560771@qq.com>
2021-10-25 17:25:49 +0800

    secondFormat

--- date-utils.js
+++ date-utils.js
@@ -17,3 +17,28 @@ export const dateFormat = (timestamp, fmt = 'yyyy-MM-dd hh:mm:ss') => {
   });
   return fmt;
 };
+
+export const secondFormat = (time, fmt = 'dd天hh时mm分ss秒', noZero = false) => {
+  const d = Math.floor(time / (60 * 60 * 24));
+  if (new RegExp('d+').test(fmt)) time -= d * 60 * 60 * 24;
+  const h = Math.floor(time / (60 * 60));
+  if (new RegExp('h+').test(fmt)) time -= h * 60 * 60;
+  const m = Math.floor(time / 60);
+  if (new RegExp('m+').test(fmt)) time -= m * 60;
+  const s = time;
+  const opt = {
+    '(d+):*-*[\\u4E00-\\u9FFF]*/*': d.toString(),
+    '(h+):*-*[\\u4E00-\\u9FFF]*/*': h.toString(),
+    '(m+):*-*[\\u4E00-\\u9FFF]*/*': m.toString(),
+    '(s+):*-*[\\u4E00-\\u9FFF]*/*': s.toString()
+  };
+  Object.keys(opt).forEach((key) => {
+    fmt = fmt.replace(new RegExp(key), (value, match) => {
+      if (noZero && opt[key] === '0') return '';
+      return match.length > opt[key].length
+        ? value.replace(match, opt[key].padStart(match.length, '0'))
+        : value.replace(match, opt[key]);
+    });
+  });
+  return fmt;
+};
