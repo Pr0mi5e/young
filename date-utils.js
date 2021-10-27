975e61d9d6342f47d8625e3c7b7f4b25c643027d
Gaoxiang <512560771@qq.com>
2021-10-27 17:20:42 +0800

    tree

--- date-utils.js
+++ date-utils.js
@@ -42,3 +42,38 @@ export const secondFormat = (time, fmt = 'dd天hh时mm分ss秒', noZero = false)
   });
   return fmt;
 };
+
+const treeToMap = (tree, childNode = 'children', valueKey = 'id') => {
+  let res = {};
+  if (!tree || tree.constructor.name !== 'Object') return res;
+  if (Object.keys(tree).length === 0) return res;
+  const children = tree[childNode];
+  Reflect.deleteProperty(tree, [childNode]);
+  if (tree[valueKey] === void(0)) throw new Error(`property '${valueKey}' is not define in ${JSON.stringify(tree)}`); 
+  res[tree[valueKey]] = tree;
+  if (children && children.length > 0) {
+    children.forEach((v) => {
+      Object.assign(res, treeToMap(v, childNode, valueKey))
+    });
+    return res;
+  } else {
+    return res;
+  }
+};
+
+const treeToArray = (tree, childNode = 'children') => {
+  let res = [];
+  if (!tree || tree.constructor.name !== 'Object') return res;
+  if (Object.keys(tree).length === 0) return res;
+  const children = tree[childNode];
+  Reflect.deleteProperty(tree, [childNode]);
+  res.push(tree);
+  if (children && children.length > 0) {
+    children.forEach((v) => {
+      res = res.concat(treeToArray(v, childNode));
+    });
+    return res;
+  } else {
+    return res;
+  }
+}
