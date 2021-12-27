bd5a4b4e1a82885bd645db6f284c4d651fb72901
Gaoxiang <512560771@qq.com>
2021-12-27 10:01:20 +0800

    冒泡排序，utils

---
+++ bubbling-sort.js
@@ -0,0 +1,17 @@
+/**
+ * 冒泡排序
+ * @param {} arr
+ * @returns 
+ */
+const bubblingSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = arr.length - 1; j > i; j--) {
+      if (arr[j] > arr[j - 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
--- utils.js
+++ utils.js
@@ -25,7 +25,7 @@ export const throttle = (fn, ms = 500) => {
   };
 }
 
-const treeToMap = (tree, childNode = 'children', valueKey = 'id') => {
+export const treeToMap = (tree, childNode = 'children', valueKey = 'id') => {
   let res = {};
   if (!tree || tree.constructor.name !== 'Object') return res;
   if (Object.keys(tree).length === 0) return res;
@@ -43,7 +43,7 @@ const treeToMap = (tree, childNode = 'children', valueKey = 'id') => {
   }
 };
 
-const treeToArray = (tree, childNode = 'children') => {
+export const treeToArray = (tree, childNode = 'children') => {
   let res = [];
   if (!tree || tree.constructor.name !== 'Object') return res;
   if (Object.keys(tree).length === 0) return res;
@@ -59,3 +59,24 @@ const treeToArray = (tree, childNode = 'children') => {
     return res;
   }
 }
+
+/**
+ * 获取文本宽度
+ * @param {*} font 
+ * @returns 
+ */
+export const getTextWidth = (font) => {
+  const canv = document.createElement('canvas');
+  const context = canv.getContext('2d');
+  context.font = font; // '14px PingFang SC'
+  return context.measureText(text).width;
+};
+
+/**
+ * 获取Dom高度
+ * @param {*} dom 
+ * @returns 
+ */
+export const getDomHeigth = (dom) => {
+  return window.getComputedStyle(dom).height;
+};
